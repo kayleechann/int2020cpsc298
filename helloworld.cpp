967dc285131a35793b1f95a6880471bf057ae304
root <kaychan@chapman.edu>
2020-01-14 20:53:57 +0000

    i made additional changes

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main(){
   cout << "Hello World!" << endl;
-
+  cout << "I had to add this statement cause Rene said so";
+  cout << "adding another cout" << endl;
+  
   return 0;
 }
